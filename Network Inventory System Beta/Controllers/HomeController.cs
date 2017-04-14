3672c9ea1e776d3b8a0313cc642825cb84488f8d
Matt West <stillmattwest@gmail.com>
2017-04-14 11:07:07 -0700

    Serch data persists during re-order in home / index view

--- Network Inventory System Beta/Controllers/HomeController.cs
+++ Network Inventory System Beta/Controllers/HomeController.cs
@@ -12,8 +12,16 @@ namespace Network_Inventory_System_Beta.Controllers
     {
         private InventoryContext db = new InventoryContext();
         
-        public ViewResult Index(string locationString,string manString,string modelString,string snString)
+        public ViewResult Index(string locationString,string manString,string modelString,string snString,string sortOrder)
         {
+            
+            // persist search data
+            ViewBag.locationString = locationString;
+            ViewBag.manString = manString;
+            ViewBag.modelString = modelString;
+            ViewBag.snString = snString;
+
+
             IQueryable<InventoryItem> resultItems = db.InventoryItems;
 
             if (!String.IsNullOrEmpty(snString))
@@ -43,6 +51,28 @@ namespace Network_Inventory_System_Beta.Controllers
             {
                 resultItems = resultItems
                     .Where(i => i.ModelNumber.ToLower().Contains(modelString.ToLower()));
+            }
+            
+            switch (sortOrder)
+            {
+                case "location":
+                    resultItems = resultItems.OrderBy(i => i.Location);
+                    break;
+                case "sn":
+                    resultItems = resultItems.OrderBy(i => i.SerialNumber);
+                    break;
+                case "man":
+                    resultItems = resultItems.OrderBy(i => i.Manufacturer);
+                    break;
+                case "model":
+                    resultItems = resultItems.OrderBy(i => i.ModelNumber);
+                    break;
+                case "asset":
+                    resultItems = resultItems.OrderBy(i => i.AssetNumber);
+                    break; 
+                default:
+                    resultItems = resultItems.OrderBy(i => i.ItdId);
+                    break;
             }
             
             
